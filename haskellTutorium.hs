92950bf99202635209be6037bd69ad03e8a7011c
Mehmed Maljoki <meski@Mehmeds-MacBook-Pro.local>
2018-10-13 23:21:41 +0200

    huskell add

---
+++ haskellTutorium.hs
@@ -0,0 +1,21 @@
+import Data.List
+import System.IO
+
+
+-- Int -2^63 2^63
+maxInt = maxBound :: Int
+minInt = minBound :: Int
+
+-- Integer - so viel wie Memory hat
+-- Float oder Double
+
+bigFloat = 3.999999999999 + 0.0000000005
+
+-- Bool True False
+-- Char
+-- Tuple
+always5 :: IntÅ
+always5 = 5
+
+-- Functions
+sumOfNums = sum [1..1000]
